2198ab69195b368cb6fcc054410d0059c6c98281
Balbir Singh <rathorebalbir22@gmail.com>
2024-03-01 16:09:52 +0530

    has added programme

---
+++ src/main/kotlin/geeksforgeeks/LetterCombinationOfPhoneNumber.kt
@@ -0,0 +1,61 @@
+package org.example.geeksforgeeks
+
+import java.util.*
+
+
+internal object GFG {
+    // Function to return a vector that contains
+    // all the generated letter combinations
+    private fun letterCombinationsUtil(digits: String, n: Int,
+                                       table: Array<String>
+    ): ArrayList<String> {
+        // To store the generated letter combinations
+        val list = ArrayList<String>()
+        //val queue: Queue<String> = LinkedList()
+        val queue : Queue<String> = LinkedList()
+        queue.add("")
+        while (!queue.isEmpty() && digits.isNotEmpty()) {
+            val s = queue.remove()
+            // If complete word is generated
+            // push it in the list
+            if (s.length == n)
+                list.add(s)
+            else {
+                val value = table[digits[s.length].digitToInt()]
+                for (element in value) {
+                    queue.add(s + element)
+                }
+            }
+        }
+        return list
+    }
+
+    // Function that creates the mapping and
+    // calls letterCombinationsUtil
+    private fun letterCombinations(digits: String, n: Int) {
+        // table[i] stores all characters that
+        // corresponds to ith digit in phone
+        val table = arrayOf(
+            "0", "1", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        )
+
+        val list = letterCombinationsUtil(digits, n, table)
+        // Print the contents of the list
+        println(list)
+        for (i in list.indices) {
+            print(list[i] + " ")
+        }
+    }
+
+    // Driver code
+    @JvmStatic
+    fun main(args: Array<String>) {
+        //val number = intArrayOf(2, 3)
+        //val n = number.size
+        val digits = "23"
+        val n1 = digits.length
+        // Function call
+        letterCombinations(digits, n1);
+    }
+}
---
+++ src/main/kotlin/geeksforgeeks/ProductArray.kt
@@ -0,0 +1,146 @@
+package org.example.geeksforgeeks
+
+
+internal class ProductArray {
+    /* Function to print product array
+    for a given array arr[] of size n */
+    fun productArray1(arr: IntArray, n: Int) {
+        // Base case
+        if (n == 1) {
+            print(0)
+            return
+        }
+        // Initialize memory to all arrays
+        val left = IntArray(n)
+        val right = IntArray(n)
+        val prod = IntArray(n)
+
+
+        /* Left most element of left array is always 1 */
+        left[0] = 1
+
+
+        /* Right most element of right array is always 1 */
+        right[n - 1] = 1
+
+
+        // 1, 10, 30, 150, 900,
+        /* Construct the left array */
+        var i = 1
+        while (i < n) {
+            left[i] = arr[i - 1] * left[i - 1]
+            print("${left[i]}, ")
+            i++
+
+            //print(prod[i].toString() + " ")
+
+        }
+
+
+        /* Construct the right array */
+        var j = n - 2
+        println()
+        while (j >= 0) {
+            right[j] = arr[j + 1] * right[j + 1]
+            print("${right[j]}, ")
+            j--
+        }
+
+
+
+
+
+
+        /* Construct the product array using
+        left[] and right[] */
+        i = 0
+        while (i < n) {
+            prod[i] = left[i] * right[i]
+            i++
+        }
+
+        println()
+
+        /* print the constructed prod array */
+        i = 0
+        while (i < n) {
+            print(prod[i].toString() + " ")
+            i++
+        }
+
+        return
+    }
+
+
+    fun productArray(arr: IntArray, n: Int) {
+        // Base case
+        val size = arr.size
+
+        if (n == 1) {
+            print("0")
+            return
+        }
+        var temp = 1
+
+
+        /* Allocate memory for the product array */
+        val prod = IntArray(n)
+
+
+        /* Initialize the product array as 1 */
+        for (j in 0 until n) prod[j] = 1
+
+
+        //(10, 3, 5, 6, 2)
+        /* In this loop, temp variable contains product of
+           elements on left side excluding arr[i] */
+        var i = 0
+        while (i < n) {
+            prod[i] = temp
+            temp *= arr[i]
+            print("${prod[i]},")
+            i++
+        }
+
+        println()
+        /* Initialize temp to 1 for product on right side */
+        temp = 1
+
+
+        /* In this loop, temp variable contains product of
+           elements on right side excluding arr[i] */
+        //(10, 3, 5, 6, 2)
+        i = n - 1
+        while (i >= 0) {
+            prod[i] *= temp
+            temp *= arr[i]
+            print("${prod[i]},")
+            i--
+        }
+
+
+        /* print the constructed prod array */
+        println()
+        i = 0
+        while (i < n) {
+            print(prod[i].toString() + " ")
+            i++
+        }
+
+        return
+    }
+
+    companion object {
+        /* Driver program to test the above function */
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val pa = ProductArray()
+           //val  nums = intArrayOf(-1,1,0,-3,3)
+            val nums = intArrayOf(10, 3, 5, 6, 2)
+            val n =  nums.size
+            //println("The product array is : ")
+            pa.productArray(nums, n)
+            val array = arrayOfNulls<Int>(5)
+        }
+    }
+}
